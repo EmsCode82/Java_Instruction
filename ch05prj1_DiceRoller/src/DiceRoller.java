cc9f391137c306534f4913a8a5e7a41f5cf0e31f
EmsCode82 <emsgithub@gmail.com>
2020-03-26 16:40:11 -0400

    .

---
+++ ch05prj1_DiceRoller/src/DiceRoller.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class DiceRoller {
+
+	public static void main(String[] args) {
+		System.out.println("Dice Roller");
+		System.out.println();
+		System.out.println("Roll the Dice? y/n");
+        Scanner sc = nextInt();
+	}
+	public static int("") {
+
+}
---
+++ ch05prj2_DataValidation/src/DataValidation.java
@@ -0,0 +1,9 @@
+
+public class DataValidation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
