075d5a992ea3a22477b00181e96721d487deebf4
EmsCode82 <emsgithub@gmail.com>
2020-03-26 13:37:34 -0400

    .

--- ch05_Methods/src/MethodsToMyMadness.java
+++ ch05_Methods/src/MethodsToMyMadness.java
@@ -6,15 +6,18 @@ public class MethodsToMyMadness {
 	public static void main(String[] args) {
 		System.out.println("Hello ");
 		Scanner sc = new Scanner(System.in);
+
 		String name = "Eric";
 		printWelcomeMessage();
 		printWelcomeMessage(name);
 		printWelcomeMessage("Steven");
+
 		// get 2 integers from one line of input
-		System.out.println("Enter 2 integers (ex. 1 5); ");
-		//int a = sc.nextInt();
-		//int b = sc.nextInt();
-	    // get int a
+		// System.out.println("Enter 2 integers (ex. 1 5); ");
+		// int a = sc.nextInt();
+		// int b = sc.nextInt();
+
+		// get int a
 		int a = getValidInt("Enter number 1: ", sc);
 
 		// get int b
@@ -22,27 +25,31 @@ public class MethodsToMyMadness {
 
 		System.out.println("Sum is " + sum(a, b));
 		System.out.println("Difference is " + diff(a, b));
+		System.out.println("Quotient is " + quotient(a, b));
+		System.out.println("Product is " + product(a, b));
+
 		System.out.println("Bye");
 
 	}
 
-	private static int getValidInt(String prompt,Scanner sc) {
+	private static int getValidInt(String prompt, Scanner sc) {
 		int i = 0;
 		while (true) {
-		//print message to the user
-		System.out.print(prompt);
-		try { 
-			//get user input
-			i = sc.nextInt();
-			break;
+			// print message to the user
+			System.out.print(prompt);
+			try {
+				   // get user input
+				   i = sc.nextInt();
+				   break;
+			} 
+			catch (InputMismatchException ime) {
+				    // check if there's an exception
+				    System.out.println("Error. Entry must be a whole number.");
+				    sc.nextLine(); // discard any input
+			}
+			// re-prompt if exception
 		}
-		catch (InputMismatchException ime) {
-			//check if there's an exception
-			System.out.println("Error. Entry must be a whole number");
-		}
-		//re-prompt if exception
-		}
-		//return valid input
+		// return valid input
 		return i;
 	}
 
@@ -52,6 +59,7 @@ public class MethodsToMyMadness {
 
 	private static void printWelcomeMessage(String name) {
 		System.out.println("Welcome, " + name + ", to my madness!");
+		
 	}
 
 	private static int sum(int n1, int n2) {
@@ -60,6 +68,14 @@ public class MethodsToMyMadness {
 
 	private static int diff(int a, int b) {
 		return a - b;
+	}
+
+	private static int product(int a, int b) {
+		return a * b;
+	}
+
+	private static int quotient(int a, int b) {
+		return a / b;
 	}
 
 }
