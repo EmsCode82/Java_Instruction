87fa494683a9554f6f1888b1a77eb76580b48295
EmsCode82 <emsgithub@gmail.com>
2020-04-12 12:09:26 -0400

    .

--- PetStore/src/ui/PetStoreApp.java
+++ PetStore/src/ui/PetStoreApp.java
@@ -13,25 +13,21 @@ public class PetStoreApp {
 		System.out.println(
 				"This application will manage a list of pets. \nYou can add pets to the inventory, and remove them when they get adopted.");
 		displayMenu();
-		String command = "";
-		while (!command.equalsIgnoreCase("exit")) {
-			command = Console.getString("Command?: ", true);
-		}
 		String action = "";
 		while (!action.equalsIgnoreCase("exit")) {
 			action = Console.getString("Enter a command: ");
-			System.out.println();
 			if (action.equalsIgnoreCase("list")) {
 				displayAllPets();
 			} else if (action.equalsIgnoreCase("add")) {
 				addPet();
 			} else if (action.equalsIgnoreCase("adopt") || action.equalsIgnoreCase("delete")) {
-			//    adoptPet();
+				// adoptPet();
 			} else if (action.equalsIgnoreCase("exit")) {
 				System.out.println("Bye.\n");
 			} else {
 				System.out.println("Error! Not a valid command.\n");
 			}
+
 		}
 	}
 
@@ -52,7 +48,7 @@ public class PetStoreApp {
 		pets.add(p1);
 		pets.add(p2);
 		for (Pet pet : pets) {
-			System.out.println(pets);
+			System.out.println(pet);
 		}
 	}
 
@@ -61,7 +57,7 @@ public class PetStoreApp {
 		String species = Console.getString("Enter Species: ");
 		String name = Console.getString("Enter name: ");
 		int age = Console.getInt("Enter age: ");
-        Pet pet = new Pet();
+		Pet pet = new Pet();
 		pet.setType(type);
 		pet.setSpecies(species);
 		pet.setName(name);
@@ -69,11 +65,14 @@ public class PetStoreApp {
 		System.out.println();
 		System.out.println(type + " " + name + " " + species + " has been added.\n");
 	}
+
+	public static void adoptPet() { 
+        String pet = Console.getString("Id of pet to adopt? "); 
+        System.out.println();
+        Pet p = 
+        if (p != null) { 
+	    System.out.println(p.getName() + " has been deleted.\n"); }
+    else { 
+	    System.out.println("No pets match that Id.\n"); }
+  }
 }
-/*
- * public static void adoptPet() { String pet =
- * Console.getString("Id of pet to adopt? "); System.out.println();
- * 
- * if (p != null) { System.out.println(p.getName() + " has been deleted.\n"); }
- * else { System.out.println("No pets match that Id.\n"); } }
- */
